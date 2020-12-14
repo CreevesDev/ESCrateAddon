7fc263c8b729e93b6ca243a11fbf63ac9e182653
Xemorr <31805746+Xemorr@users.noreply.github.com>
2020-12-14 15:51:16 +0000

    Made main class nicer.

--- src/main/java/me/cooldcb/escrateaddon/ESCrateAddon.java
+++ src/main/java/me/cooldcb/escrateaddon/ESCrateAddon.java
@@ -4,24 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ESCrateAddon extends JavaPlugin implements Listener {
+public final class ESCrateAddon extends JavaPlugin {
+
+    Listener[] listeners = new Listener[] {new CrateEvents(this)};
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
-        Listener[] listeners = new Listener[] {
-            this,
-            new CrateEvents(this)
-        };
-
         registerEvents(listeners);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 
     public void registerEvents(Listener[] listeners) {
         for (Listener listener : listeners) {
